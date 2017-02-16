9c4cdad3a3a8f151af68479ba7c779b04d094b40
Wileydj <Wileydj@users.noreply.github.com>
2017-02-15 19:34:28 -0800

    Readability
    Added whitespace for readability.

--- parse_this.py
+++ parse_this.py
@@ -8,12 +8,18 @@ next(sampleDataReader) #skip the first line (contains information which is not i
 
 sortedByInts = sorted(sampleDataReader, key = lambda x: int(x[1])) #sort the data according to the elements in column 2, according to their ascending value
 
-
 i = 0 #establishing a counter
 
+
 for i in range(5000): # establishing a range which represents the entire data sheet
+	
 	if int(sortedByInts[i][1]) < 1405987200 and int(sortedByInts[i][1]) > 1403395200: #selects only data which exists between the dates June, 22, 2014 and July 22, 2014
+		
 		print(sortedByInts[i][8]) #prints any data in the 8th column which is between those timestamps
+		
+		
 		i += 1 #increments after printing
+		
 	else:
+		
 		i += 1 #increments even if we don’t print!
